a9811bc0afc7e79e20a740c2829eeec8ba378b8a
RyunGeum <131639123+RyunGeum@users.noreply.github.com>
2024-04-18 13:09:14 +0900

    Update SingletonExample.java

--- src/main/java/kr/easw/lesson05/SingletonExample.java
+++ src/main/java/kr/easw/lesson05/SingletonExample.java
@@ -1,6 +1,5 @@
 package kr.easw.lesson05;
 
-
 /**
  * 다음 코드는 싱글톤 패턴을 구현한 예제입니다.
  *
@@ -10,9 +9,10 @@ package kr.easw.lesson05;
  * 싱글톤 클래스는 보통 유틸리티 클래스에 사용되며, 외부에서 해당 클래스의 상속 및 인스턴스 생성을 원치 않는 경우에도 사용합니다.
  */
 public class SingletonExample {
+
     // 싱글톤 클래스의 인스턴스는 private static final으로 선언합니다.
     // 보통 지정된 클래스에 하나의 인스턴스만을 선언하며, 일반적인 경우는 이 클래스가 싱글톤임을 알리기 위해 변수명을 대문자로 사용합니다.
-    public static final PerformanceImpactingSomething SOMETHING = new PerformanceImpactingSomething();
+    private static final PerformanceImpactingSomething SOMETHING = new PerformanceImpactingSomething();
 
     public static void main(String[] args) {
         System.out.println(SOMETHING.doSomething());
@@ -26,8 +26,8 @@ public class SingletonExample {
         private String doSomething() {
             try {
                 Thread.sleep(5000L);
-            } catch (Exception ex) {
-                // 예외가 발생할 일이 존재하지 않습니다. 무시합니다..
+            } catch (InterruptedException ex) { // InterruptedException 예외 처리를 추가합니다.
+                Thread.currentThread().interrupt(); // 예외 발생 시 인터럽트 상태를 다시 설정합니다.
             }
             return "Hello, World!";
         }
